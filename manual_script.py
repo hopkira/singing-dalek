eee0217e04eccdff74ee1ee265ed4faf134179f4
Richard Hopkins <hopkira@googlemail.com>
2021-12-05 14:48:56 +0000

    Update manual_script.py

--- manual_script.py
+++ manual_script.py
@@ -5,11 +5,15 @@ Simple script combining animation with choral singing
 import dalek_anim as DalekBody
 import singing_dalek as DalekVoice
 import time
-
-# Close eyes
+import argparse
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input", type=str, default='jinglebells.csv',
+    help="CSV input filename for song")
+args = vars(ap.parse_args())
 DalekBody.dalek_status(False)
 time.sleep(5)
 DalekBody.dalek_status(True)
-DalekVoice.sing("jinglebells.csv")
+DalekVoice.sing(args['input'])
 time.sleep(1)
 DalekBody.dalek_status(False)
